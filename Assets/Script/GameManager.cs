7c43849ca6a8e01773ff4c725dadf013b93d8167
Tanaka Hyuuji <nkc.hyuuji.tanaka@gmail.com>
2017-10-26 10:43:58 +0900

    透明なオブジェクトを飛ばして消す機能

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    public GameObject SpherePrefab;
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject Sphere=
+                Instantiate(SpherePrefab)
+                as GameObject;
+            Sphere.GetComponent<get>().Shoot(
+                new Vector3( -500, 0, 0));
+        }
+	}
+}
---
+++ Assets/Script/get.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class get : MonoBehaviour
+{
+    Vector3 setpotion;
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        DestroyObject(gameObject);
+    }
+    // Use this for initialization
+    void Start()
+    {
+        setpotion = this.gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.magnitude > 10.0f)
+        {
+            Destroy(gameObject);    // 自分自身を削除
+        }
+    }
+}
